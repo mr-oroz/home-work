c49085c14658d4953dffc035abc09368b6cea14e
Ороз Жумабеков <orozzumabekov@MacBook-Air-Oroz.local>
2022-08-31 21:00:13 +0600

    tz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Battery from "./components/battery";
+import Slider from './components/slider';
+import Buttons from './components/buttons';
+import Menu from './components/menu';
+import Card from './components/card';
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        {/* <Battery/> */}
+        {/* <Slider/> */}
+        {/* <Buttons/> */}
+        {/* <Menu/> */}
+        {/* <Card/> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
